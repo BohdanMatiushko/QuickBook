e8131ff265bba66cea742be533c54def338ab017
Andrew <andrijkravcik95@gmail.com>
2026-05-06 02:02:00 +0300

    lab 6

--- users/models.py
+++ users/models.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    first_name = models.CharField(max_length=40, blank=True, null=True, verbose_name="Ім'я")
+    last_name = models.CharField(max_length=40, blank=True, null=True, verbose_name="Фамілія")
+    password = models.CharField(max_length=255, blank=True, null=True, verbose_name="Пароль")
+    email = models.EmailField(unique=True, blank=True, null=True, verbose_name='Електронна-адреса')
     phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name="Номер телефону")
     is_client = models.BooleanField(default=True, verbose_name="Клієнт")
     is_employee = models.BooleanField(default=False, verbose_name="Співробітник")
